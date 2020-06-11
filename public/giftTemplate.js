758441fd85365baf8245bec3e454dc9518836d3c
Andrew Dassonville <dassonville.andrew@gmail.com>
2020-06-11 11:36:10 -0700

    Add trending gifts

--- public/giftTemplate.js
+++ public/giftTemplate.js
@@ -8,16 +8,16 @@ templates['giftTemplate'] = template({"compiler":[8,">= 4.3.0"],"main":function(
         return undefined
     };
 
-  return "<article class=\"gift\">\n        <div class=\"gift-content\">\n          <img class = \"gift-picture\" src = \""
-    + alias4(((helper = (helper = lookupProperty(helpers,"giftURL") || (depth0 != null ? lookupProperty(depth0,"giftURL") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"giftURL","hash":{},"data":data,"loc":{"start":{"line":3,"column":45},"end":{"line":3,"column":56}}}) : helper)))
-    + "\"/>\n\n          <p class=\"gift-description\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"description") || (depth0 != null ? lookupProperty(depth0,"description") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data,"loc":{"start":{"line":6,"column":12},"end":{"line":6,"column":27}}}) : helper)))
-    + "\n          </p>\n\n          <p class=\"gift-price\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"price") || (depth0 != null ? lookupProperty(depth0,"price") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data,"loc":{"start":{"line":10,"column":12},"end":{"line":10,"column":21}}}) : helper)))
-    + "\n          </p>\n          \n          <p class=\"gift-rating\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"rating") || (depth0 != null ? lookupProperty(depth0,"rating") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"rating","hash":{},"data":data,"loc":{"start":{"line":14,"column":12},"end":{"line":14,"column":22}}}) : helper)))
-    + "\n          </p>\n\n          <p class=\"tags\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"tags") || (depth0 != null ? lookupProperty(depth0,"tags") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"tags","hash":{},"data":data,"loc":{"start":{"line":18,"column":12},"end":{"line":18,"column":20}}}) : helper)))
-    + "\n          </p>\n        </div>\n      </article>";
+  return "<article class=\"gift\">\n  <div class=\"gift-content\">\n    <div class=\"gift-items\">\n      <p class=\"gift-description\">\n        "
+    + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":5,"column":8},"end":{"line":5,"column":17}}}) : helper)))
+    + "\n      </p>\n\n      <p class=\"gift-price\">\n        $"
+    + alias4(((helper = (helper = lookupProperty(helpers,"price") || (depth0 != null ? lookupProperty(depth0,"price") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data,"loc":{"start":{"line":9,"column":9},"end":{"line":9,"column":18}}}) : helper)))
+    + "\n      </p>\n\n      <p class=\"gift-rating\">\n        "
+    + alias4(((helper = (helper = lookupProperty(helpers,"rating") || (depth0 != null ? lookupProperty(depth0,"rating") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"rating","hash":{},"data":data,"loc":{"start":{"line":13,"column":8},"end":{"line":13,"column":18}}}) : helper)))
+    + "\n      </p>\n\n      <p class=\"tags\">\n        "
+    + alias4(((helper = (helper = lookupProperty(helpers,"tags") || (depth0 != null ? lookupProperty(depth0,"tags") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"tags","hash":{},"data":data,"loc":{"start":{"line":17,"column":8},"end":{"line":17,"column":16}}}) : helper)))
+    + "\n      </p>\n    </div>\n\n    <img class=\"gift-picture\" src=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"image") || (depth0 != null ? lookupProperty(depth0,"image") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data,"loc":{"start":{"line":21,"column":35},"end":{"line":21,"column":44}}}) : helper)))
+    + "\" />\n  </div>\n</article>";
 },"useData":true});
 })();
--- server.js
+++ server.js
@@ -7,7 +7,31 @@ const Api = require('./api');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+let trending = {}
+function updateTrending(items) {
+  items.forEach(i => {
+    if (trending[i.itemId]) {
+      trending[i.itemId].score += 1;
+    } else {
+      trending[i.itemId] = {
+        ...i,
+        score: 1,
+      };
+    }
+  });
+
+  const newTrending = {};
+  const newTrendingPairs = Object.entries(trending).sort((a, b) => b[1].score - a[1].score).slice(0, 50);
+  newTrendingPairs.forEach(i => newTrending[i[0]] = i[1]);
+
+  trending = newTrending;
+}
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,12 +43,18 @@ app.get('/about', (req, res) => {
   res.render('aboutUs');
 });
 
+app.get('/trending', (req, res) => {
+  res.render('trending', { items: Object.values(trending).sort((a, b) => b.score - a.score) });
+});
+
 app.get('/gifts/:keyword', async (req, res) => {
   const response = await Api.getByKeywords(req.params.keyword)
   const items = response.map(i => ({
     ...i,
     image: i.pictureURLLarge || i.galleryURL,
   }))
+
+  updateTrending(items);
 
   res.render('results', { items })
 })
